9020ab89af1e7c35d9a66b0d03293f6be6908dff
Varorbc <varorbc@gmail.com>
2023-08-29 09:00:00 +0800

    init project

---
+++ GrpcService1/Program.cs
@@ -0,0 +1,25 @@
+using GrpcService1.Services;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddGrpc().AddJsonTranscoding();
+builder.Services.AddGrpcSwagger();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new OpenApiInfo { Title = "gRPC transcoding", Version = "v1" });
+
+    var filePath = Path.Combine(System.AppContext.BaseDirectory, "GrpcService1.xml");
+    c.IncludeXmlComments(filePath);
+    c.IncludeGrpcXmlComments(filePath, includeControllerXmlComments: true);
+});
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
+app.MapGrpcService<GreeterService>();
+
+app.Run();
